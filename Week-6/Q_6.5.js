f4aaf47d406f1b469757f37317fb64e06698ab49
AbhishekSagu <abhisheksagu@gmail.com>
2022-11-16 09:16:53 +0530

    submit question 6.5

---
+++ Week-6/Q_6.5.js
@@ -0,0 +1,20 @@
+const doesPairWithGivenDifferenceExists=(arr,diff)=>{
+    const map = new Map();
+
+    for(let i=0;i<arr.length;i++){
+        let temp=Math.abs(diff-arr[i]);
+        if(map.has(temp)){
+            return true;
+        }
+        else{
+            map.set(arr[i],true);
+        }
+    }
+
+    return false
+
+}
+
+const arr=[5,10,3,2,50,80];
+
+console.log("**@ result 1 is , ",doesPairWithGivenDifferenceExists(arr,46))
