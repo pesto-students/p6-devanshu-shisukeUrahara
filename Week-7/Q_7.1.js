5fa7cfa13b6e6c9242ab24d0637da68e60190c73
AbhishekSagu <abhisheksagu@gmail.com>
2022-11-23 09:09:07 +0530

    WEEK-7: Submit question 7.1

---
+++ Week-7/Q_7.1.js
@@ -0,0 +1,36 @@
+const {Node,LinkedList} = require('./LinkedList');
+
+const reverseLinkedList=(list)=>{
+
+   let previousNode=null;
+   let currentNode=list.head;
+   let nextNode=currentNode.next;
+
+   while(currentNode){
+    nextNode=currentNode.next;
+    currentNode.next=previousNode;
+    previousNode=currentNode;
+    currentNode=nextNode;
+   }
+
+   list.head=previousNode;
+}
+
+
+const node1= new Node(1);
+const node2= new Node(2);
+const node3= new Node(3);
+const node4= new Node(4);
+const node5= new Node(5);
+
+node1.next=node2;
+node2.next=node3;
+node3.next=node4;
+node4.next=node5;
+
+const list=new LinkedList(node1);
+list.printList();
+console.log("***********************************************")
+reverseLinkedList(list);
+list.printList();
+
